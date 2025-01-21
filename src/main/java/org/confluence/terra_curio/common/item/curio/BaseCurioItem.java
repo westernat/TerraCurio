475b83c1efb0371e715403f42ead01ab3caa071a
westernat <westernat233@163.com>
2025-01-21 10:00:35 +0800

    允许饰品打上诅咒

--- src/main/java/org/confluence/terra_curio/common/item/curio/BaseCurioItem.java
+++ src/main/java/org/confluence/terra_curio/common/item/curio/BaseCurioItem.java
@@ -6,6 +6,7 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.EnchantmentTags;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
@@ -13,6 +14,7 @@ import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import org.confluence.terra_curio.TerraCurio;
@@ -134,6 +136,11 @@ public class BaseCurioItem extends Item implements ICurioItem {
 
     public static Builder builder(String name) {
         return new Builder(name, new Properties());
+    }
+
+    @Override
+    public boolean supportsEnchantment(ItemStack stack, Holder<Enchantment> enchantment) {
+        return enchantment.is(EnchantmentTags.CURSE);
     }
 
     public static class Builder {
