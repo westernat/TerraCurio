30468352bcf84dfa27aad116e0c59ad39b0b6fa3
westernat <westernat233@163.com>
2025-02-08 12:01:04 +0800

    调整创造模式标签页

--- src/main/java/org/confluence/terra_curio/common/init/TCTabs.java
+++ src/main/java/org/confluence/terra_curio/common/init/TCTabs.java
@@ -3,16 +3,15 @@ package org.confluence.terra_curio.common.init;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.confluence.terra_curio.TerraCurio;
 import org.confluence.terra_curio.util.TCUtils;
 
-import java.util.function.Supplier;
-
 public final class TCTabs {
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TerraCurio.MODID);
 
-    public static final Supplier<CreativeModeTab> ACCESSORIES = TABS.register("accessories",
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ACCESSORIES = TABS.register("accessories",
             () -> CreativeModeTab.builder().icon(() -> TCItems.ICON.get().getDefaultInstance())
                     .title(Component.translatable("creativetab.terra_curio"))
                     .displayItems((parameters, output) -> {
