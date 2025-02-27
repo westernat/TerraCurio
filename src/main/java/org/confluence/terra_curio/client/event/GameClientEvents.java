e07f4c9929066f9a4a97763c03beb5791b7c5722
westernat <westernat233@163.com>
2025-02-27 13:38:37 +0800

    微调

--- src/main/java/org/confluence/terra_curio/client/event/GameClientEvents.java
+++ src/main/java/org/confluence/terra_curio/client/event/GameClientEvents.java
@@ -8,7 +8,6 @@ import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.FormattedText;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
-import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -23,12 +22,8 @@ import org.confluence.terra_curio.client.animate.MasterColorAnimation;
 import org.confluence.terra_curio.client.handler.*;
 import org.confluence.terra_curio.client.renderer.tooltip.MultiFunctionTooltip;
 import org.confluence.terra_curio.common.component.ModRarity;
-import org.confluence.terra_curio.common.init.TCCommonConfigs;
 import org.confluence.terra_curio.common.init.TCEffects;
-import org.confluence.terra_curio.common.init.TCItems;
 import org.confluence.terra_curio.mixin.client.accessor.MinecraftAccessor;
-import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
 
 import java.util.List;
 import java.util.Optional;
@@ -120,25 +115,12 @@ public final class GameClientEvents {
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void renderTooltip$GatherComponents(RenderTooltipEvent.GatherComponents event) {
-        LocalPlayer player = Minecraft.getInstance().player;
-        ItemStack itemStack = event.getItemStack();
-        if (player != null && itemStack.is(TCItems.DEMON_HEART.get())) {
-            CuriosApi.getCuriosInventory(player).ifPresent(iCuriosItemHandler -> {
-                ICurioStacksHandler iCurioStacksHandler = iCuriosItemHandler.getCurios().get(TerraCurio.CURIO_SLOT);
-                Component remainingTimes = Component.translatable(
-                        "tooltip.item.terra_curio.demon_heart.1",
-                        TCCommonConfigs.MAX_ACCESSORIES.get() - iCurioStacksHandler.getSlots()
-                ).withColor(0xAAAAAA);
-                event.getTooltipElements().add(Either.left(remainingTimes));
-            });
-        }
-
         List<Either<FormattedText, TooltipComponent>> tooltipElements = event.getTooltipElements();
         if (tooltipElements.isEmpty()) return;
         Optional<FormattedText> displayName = tooltipElements.getFirst().left();
         if (displayName.isPresent() && displayName.get() instanceof Component component) {
             tooltipElements.set(0, Either.left(
-                    component.copy().withColor(ModRarity.getRarity(itemStack).getColor())
+                    component.copy().withColor(ModRarity.getRarity(event.getItemStack()).getColor())
             ));
         }
     }
