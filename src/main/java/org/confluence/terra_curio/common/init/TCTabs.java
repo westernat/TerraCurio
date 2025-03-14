84438204c2d3f82cc211079d399d86d0c71d6905
westernat <westernat233@163.com>
2025-03-14 23:10:53 +0800

    创造模式标签页进行双向绑定

--- src/main/java/org/confluence/terra_curio/common/init/TCTabs.java
+++ src/main/java/org/confluence/terra_curio/common/init/TCTabs.java
@@ -2,6 +2,8 @@ package org.confluence.terra_curio.common.init;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -22,6 +24,9 @@ public final class TCTabs {
                         output.accept(TCItems.CELL_PHONE.get());
                         output.accept(TCItems.DIVING_HELMET.get());
                         TCItems.CURIOS.getEntries().forEach(entry -> output.accept(entry.get()));
-                    }).build()
+                    })
+                    .withTabsAfter(ResourceKey.create(Registries.CREATIVE_MODE_TAB, ResourceLocation.fromNamespaceAndPath("confluence", "armors")))
+                    .withTabsBefore(ResourceKey.create(Registries.CREATIVE_MODE_TAB, ResourceLocation.fromNamespaceAndPath("confluence", "tools")))
+                    .build()
     );
 }
