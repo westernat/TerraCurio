bcd986cac4e7942d8627bb5092c6d18a6cb044bb
westernat <westernat233@163.com>
2025-03-16 23:47:09 +0800

    时装栏饰品可以任意放入，回忆药可以跨纬度回家了

--- src/main/java/org/confluence/terra_curio/common/item/curio/BaseCurioItem.java
+++ src/main/java/org/confluence/terra_curio/common/item/curio/BaseCurioItem.java
@@ -117,7 +117,7 @@ public class BaseCurioItem extends Item implements ICurioItem {
 
     @Override
     public boolean canEquip(SlotContext slotContext, ItemStack stack) {
-        return CuriosUtils.noSameCurio(slotContext.entity(), this);
+        return slotContext.cosmetic() || CuriosUtils.noSameCurio(slotContext.entity(), this);
     }
 
     @Override
