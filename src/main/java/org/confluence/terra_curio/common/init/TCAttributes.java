dadbba652b01b33fc0a871d083c09775eb33ea6a
westernat <westernat233@163.com>
2025-03-24 12:49:23 +0800

    大修

--- src/main/java/org/confluence/terra_curio/common/init/TCAttributes.java
+++ src/main/java/org/confluence/terra_curio/common/init/TCAttributes.java
@@ -127,7 +127,7 @@ public final class TCAttributes {
     }
 
     public static float applyCritDamage(RandomSource random, LivingEntity living, float amount) {
-        if (hasCustomAttribute(CRIT_CHANCE)) return amount;
+        if (TerraCurio.IS_CONFLUENCE_LOADED || hasCustomAttribute(CRIT_CHANCE)) return amount;
         AttributeInstance instance = living.getAttribute(CRIT_CHANCE);
         if (instance != null && random.nextFloat() < instance.getValue()) {
             amount *= 1.5F;
