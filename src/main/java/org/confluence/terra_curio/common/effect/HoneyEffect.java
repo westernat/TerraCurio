9b1a8f7b8c2c28a9b44535aaa62757e53faac00c
westernat <westernat233@163.com>
2025-05-29 18:44:58 +0800

    加强开发者武器

--- src/main/java/org/confluence/terra_curio/common/effect/HoneyEffect.java
+++ src/main/java/org/confluence/terra_curio/common/effect/HoneyEffect.java
@@ -2,7 +2,12 @@ package org.confluence.terra_curio.common.effect;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.monster.piglin.AbstractPiglin;
+import net.minecraft.world.entity.player.Player;
+import org.confluence.terra_curio.common.init.TCEffects;
 
 @javax.annotation.ParametersAreNonnullByDefault
 @net.minecraft.MethodsReturnNonnullByDefault
@@ -20,5 +25,16 @@ public class HoneyEffect extends MobEffect {
     @Override
     public boolean shouldApplyEffectTickThisTick(int pDuration, int pAmplifier) {
         return pDuration % 10 == 0;
+    }
+
+    public static void applyHoneyEffect(LivingEntity living) {
+        if (living instanceof Animal || living instanceof Player) {
+            MobEffectInstance effect = living.getEffect(TCEffects.HONEY);
+            if (effect == null || effect.getDuration() < 220) {
+                living.addEffect(new MobEffectInstance(TCEffects.HONEY, 600));
+            }
+        } else if (living instanceof AbstractPiglin piglin) {
+            piglin.setImmuneToZombification(true);
+        }
     }
 }
