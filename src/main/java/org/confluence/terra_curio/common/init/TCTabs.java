c5b914615908bc0127de65ddf61c763aa517d583
westernat <westernat233@163.com>
2025-09-11 22:35:49 +0800

    非开发环境的创造模式物品栏不显示WIP物品

--- src/main/java/org/confluence/terra_curio/common/init/TCTabs.java
+++ src/main/java/org/confluence/terra_curio/common/init/TCTabs.java
@@ -8,7 +8,8 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import org.confluence.lib.util.LibUtils;
+import org.confluence.lib.ConfluenceMagicLib;
+import org.confluence.lib.util.WipNotDisplayOutput;
 import org.confluence.terra_curio.TerraCurio;
 
 public final class TCTabs {
@@ -18,17 +19,18 @@ public final class TCTabs {
             () -> CreativeModeTab.builder().icon(() -> TCItems.ICON.get().getDefaultInstance())
                     .title(Component.translatable("creativetab.terra_curio"))
                     .displayItems((parameters, output) -> {
-                        LibUtils.forMixin$Inject();
+                        output = new WipNotDisplayOutput(output);
                         output.accept(TCItems.WORKSHOP.get());
                         output.accept(TCItems.DEMON_HEART.get());
                         output.accept(TCItems.MAGIC_MIRROR.get());
                         output.accept(TCItems.CELL_PHONE.get());
                         output.accept(TCItems.DIVING_HELMET.get());
-                        TCItems.CURIOS.getEntries().forEach(entry -> output.accept(entry.get()));
+                        CreativeModeTab.Output finalOutput = output;
+                        TCItems.CURIOS.getEntries().forEach(entry -> finalOutput.accept(entry.get()));
                     })
-                    .withTabsAfter(ResourceKey.create(Registries.CREATIVE_MODE_TAB, ResourceLocation.fromNamespaceAndPath("confluence", "armors")))
+                    .withTabsAfter(ResourceKey.create(Registries.CREATIVE_MODE_TAB, ResourceLocation.fromNamespaceAndPath(ConfluenceMagicLib.CONFLUENCE_ID, "armors")))
                     .withTabsBefore(
-                            ResourceKey.create(Registries.CREATIVE_MODE_TAB, ResourceLocation.fromNamespaceAndPath("confluence", "tools")),
+                            ResourceKey.create(Registries.CREATIVE_MODE_TAB, ResourceLocation.fromNamespaceAndPath(ConfluenceMagicLib.CONFLUENCE_ID, "tools")),
                             CreativeModeTabs.SPAWN_EGGS
                     ).build()
     );
