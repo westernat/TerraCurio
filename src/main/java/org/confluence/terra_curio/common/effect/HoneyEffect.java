e8868e0dfd25e7e2914b518437740a116575a709
westernat <westernat233@163.com>
2025-09-15 22:44:03 +0800

    完善

--- src/main/java/org/confluence/terra_curio/common/effect/HoneyEffect.java
+++ src/main/java/org/confluence/terra_curio/common/effect/HoneyEffect.java
@@ -4,9 +4,9 @@ import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.monster.piglin.AbstractPiglin;
 import net.minecraft.world.entity.player.Player;
+import org.confluence.lib.util.LibUtils;
 import org.confluence.terra_curio.common.init.TCEffects;
 
 @javax.annotation.ParametersAreNonnullByDefault
@@ -28,7 +28,7 @@ public class HoneyEffect extends MobEffect {
     }
 
     public static void applyHoneyEffect(LivingEntity living) {
-        if (living instanceof Animal || living instanceof Player) {
+        if (LibUtils.isAnimal(living) || living instanceof Player) {
             MobEffectInstance effect = living.getEffect(TCEffects.HONEY);
             if (effect == null || effect.getDuration() < 220) {
                 living.addEffect(new MobEffectInstance(TCEffects.HONEY, 600));
