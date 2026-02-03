4d519ff3c78e40067c8cbf127af03843fb05d3d7
westernat <westernat233@163.com>
2026-02-03 21:05:18 +0800

    重写敌怪旗

--- src/main/java/org/confluence/terra_curio/common/init/TCAttributes.java
+++ src/main/java/org/confluence/terra_curio/common/init/TCAttributes.java
@@ -92,7 +92,7 @@ public final class TCAttributes {
 
     public static boolean hasCustomAttribute(Holder<Attribute> attribute) {
         Holder<Attribute> holder = MAP.get(attribute);
-        return holder != null && holder.value() != attribute.value();
+        return holder != null && holder.equals(attribute);
     }
 
     public static void registerAttribute(Holder<Attribute> attribute, BiConsumer<EntityType<? extends LivingEntity>, Holder<Attribute>> consumer) {
